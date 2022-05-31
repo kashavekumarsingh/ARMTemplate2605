eb3892ae967392c1255cdd654d516206f0f3ff9a
kashave kumar <kk0405@hotmail.com>
2022-05-31 23:50:30 +0530

    modified

--- ArmTemplates.Templates/certificateconfigure.ps1
+++ ArmTemplates.Templates/certificateconfigure.ps1
@@ -7,7 +7,7 @@ Connect-AzAccount
 
 $ResourceGroupName="armappBaseName-dev-2605"
 $WebAppName ="armappBaseName-dev-2605"
-$KeyVaultName ="/subscriptions/5761586f-99b8-4741-80df-d6143a7b7bac/resourceGroups/armappBaseName-dev-2605/providers/Microsoft.KeyVault/vaults/keyvault31054" 
+$KeyVaultName ="/subscriptions/5761586f-99b8-4741-80df-d6143a7b7bac/resourceGroups/armappBaseName-dev-2605/providers/Microsoft.KeyVault/vaults/keyvault310505" 
 $CertName="certificate1"
 
 Import-AzWebAppKeyVaultCertificate 
@@ -16,7 +16,7 @@ Import-AzWebAppKeyVaultCertificate
 -KeyVaultName $KeyVaultName
 -CertName $CertName
 
-$Thumbprint ="783E531FE7775222BDADA9D3EE03742141693FE8" 
+$Thumbprint ="BB9E5AA357431B211FACD2EE5E50218963866398" 
 $Name ="www.contoso.com"
 
 New-AzWebAppSSLBinding 
